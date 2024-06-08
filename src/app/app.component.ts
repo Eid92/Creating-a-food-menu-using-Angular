6991cf658e676ef3fa0f11da6caaa5783be23c70
Eid Almasoudi <eidsalim18@gmail.com>
2024-06-09 00:42:35 +0300

    Food Menu by Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
-
+import { NgModel } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Menu';
+  total = 0;
+  qty = 0;
+  productName = '';
+  foodmenu = [
+    { id: 1, name: "Avocado Salad", price: 60, description: "Avocado salad with tomato and cheese", img: "assets/images/Avocado_salad.jpg" },
+    { id: 2, name: "Cesar Salad", price: 40, description: "Cesar salad plate", img: "assets/images/cesar_salad_plate.jpg" },
+    { id: 3, name: "Vegan Salad", price: 30, description: "Vegan salad bowl", img: "/assets/images/Vegan_sala_ bowl.jpg" },
+    { id: 4, name: "Strawberry Salad", price: 20, description: "Strawberry, spinach and feta salad", img: "assets/images/strawberry_salad.jpg" }
+  ];
+
+  sum(event: any, price: number) {
+    const isChecked: boolean = event.target.checked;
+    if (isChecked) {
+      this.total += price;
+      this.qty += 1;
+    } else {
+      this.total -= price;
+      this.qty -= 1;
+    }
+  }
+   searchByName(products: any[]){
+       return products.filter((item) => item.name.toLowerCase().includes(this.productName.toLowerCase()));
+  }
+
 }
